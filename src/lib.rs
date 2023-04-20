83680eb2501d10931815cd154740eb31f1db13f7
Wout Hiemstra <wout.hiemstra@hotmail.com>
2023-04-20 21:14:46 +0200

    Wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,52 @@
 use std::str::FromStr;
+use std::error::Error;
+use clap::Parser;
 
-pub struct Todo {
-    pub title: String,
-    pub description: String,
-    pub prio: Priority
+#[derive(Parser)]
+pub struct Cli {
+    pub pattern: String,
+    //pub path: std::path::PathBuf
+}
+
+
+#[derive(Debug, PartialEq)]
+pub enum Operation {
+    Get,
+    GetAll,
+    Add,
+    Update,
+    Delete
+}
+
+impl FromStr for Operation {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Operation, Self::Err> {
+        match s {
+            "get" => Ok(Operation::Get),
+            "getall" => Ok(Operation::GetAll),
+            "add" => Ok(Operation::Add),
+            "update" => Ok(Operation::Update),
+            "delete" => Ok(Operation::Delete),
+            _ => Err(()),
+        }
+    }
+}
+
+impl Operation {
+    fn run(args: &[String]) -> Result<(), Box<dyn Error>> {
+        // TODO: Get action user wants to do.
+        // TODO: Check length of args
+
+        Ok(())
+     }
+    
+    pub fn determine_action(action: &Operation) {
+        match action {
+            Operation::Get => println!("Getting todos..."),
+            Operation::Add => println!("Adding todo..."),
+            _ => panic!("Something went wrong..."),
+        };
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -23,6 +66,12 @@ impl FromStr for Priority {
             _ => Err(()),
         }
     }
+}
+
+pub struct Todo {
+    pub title: String,
+    pub description: String,
+    pub prio: Priority
 }
 
 impl Todo {
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
 use std::env;
-use std::process;
-
-use todo::Todo;
+use clap::Parser;
+use todo::Cli;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    //let args: Vec<String> = env::args().collect();
+    let args = Cli::parse();
 
-    let todo = Todo::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
-
-    println!("Todo title: {}", todo.title);
-    println!("Todo desc: {}", todo.description);
-    println!("Todo prio: {:?}", todo.prio);
+    println!("{:?}", args.pattern);
 }
